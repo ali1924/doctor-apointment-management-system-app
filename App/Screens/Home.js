7e40868ea33ca66d11188165b7dfb28cc9730dde
ali1924 <mohammadali229987@gmail.com>
2023-12-04 20:09:34 +0600

    logo and heading added in Home

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import Header from "../Components/Header";
 
@@ -9,6 +9,32 @@ export default function Home() {
         title={"Doctor Appointment Booking"}
         icon={require("../../assets/images/logo.png")}
       />
+
+      <Image
+        source={require("../../assets/images/banner.jpg")}
+        style={styles.banner}
+      />
+
+      <Text style={styles.heading}>Select Category</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  banner: {
+    width: "90%",
+    height: 200,
+    borderRadius: 10,
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  heading: {
+    color: "#000",
+    fontSize: 18,
+    fontWeight: "700",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+});

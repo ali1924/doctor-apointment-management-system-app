4d6adccf1649ca7ece54fcf46e10c2137adbcc49
ali1924 <mohammadali229987@gmail.com>
2023-12-04 22:23:26 +0600

    Abailable Slot done

--- App/Screens/BookAppointment.js
+++ App/Screens/BookAppointment.js
@@ -1,8 +1,11 @@
 import { View, Text, Image, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header";
+import { FlatList } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 export default function BookAppointment() {
+  const [selectedSlot, setSelectedSlot] = useState("");
   return (
     <View style={styles.container}>
       <Header
@@ -16,6 +19,44 @@ export default function BookAppointment() {
       <Text style={styles.docName}>Samia Nasir Nira</Text>
       <Text style={styles.docSpecialist}>Cardiologist</Text>
       <Text style={styles.heading}>Available Slot</Text>
+
+      <View>
+        <FlatList
+          numColumns={2}
+          data={[
+            "08:00AM-10:00AM",
+            "10:00AM-12:00 PM",
+            "12:00PM-02:00PM",
+            "02:00PM-4:00PM",
+            "04:00PM-6:00PM",
+            "06:00PM-8:00PM",
+          ]}
+          renderItem={({ item, index }) => {
+            return (
+              <TouchableOpacity
+                style={[
+                  styles.timeSlot,
+                  {
+                    borderColor: selectedSlot === index ? "green" : "black",
+                    backgroundColor: selectedSlot === index ? "green" : "white",
+                  },
+                ]}
+                onPress={() => {
+                  setSelectedSlot(index);
+                }}
+              >
+                <Text
+                  style={{
+                    color: selectedSlot === index ? "white" : "black",
+                  }}
+                >
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            );
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 }
@@ -50,5 +91,15 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginTop: 15,
     marginLeft: 15,
+  },
+  timeSlot: {
+    width: "40%",
+    height: 40,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    // backgroundColor:'',
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
